60ed3fe46a46354d6d72d3ddda73a27d689d87bb
Gin <eric.prototype@gmail.com>
2019-07-23 10:44:49 +0800

    clean up

--- src/index.js
+++ src/index.js
@@ -1,8 +1,13 @@
 import _ from "lodash"
+import printMe from "./print"
 
 function component() {
   const element = document.createElement("div")
+  const btn = document.createElement("button")
   element.innerHTML = _.join(["Hello", "webpack"], " ") //eslint-disable-line
+  btn.innerHTML = "CLick me and check the console!"
+  btn.addEventListener("click", printMe)
+  element.appendChild(btn)
   return element
 }
 
